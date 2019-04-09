1c9662672b8e4bbaf4dd061cbda842b84ee9524b
Jtejero8 <jorgetley1995@gmail.com>
2019-04-09 17:27:08 +0200

    update footer
    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HeaderComponent } from '../app/componentes/header/header.componet';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../app/componentes/header/header.componet';
+import { BodyComponent } from '../app/componentes/body/body.component';
+
 import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
